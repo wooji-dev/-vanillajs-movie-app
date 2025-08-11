77bee7b2060fd6aeaea49eb1ca073fc9418c8a3b
우정인 <wji0116@echomarketing.co.kr>
2025-08-11 16:41:23 +0900

    250811, footer 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Component } from "./core/heropy.js";
 import TheHeader from "./components/TheHeader.js";
+import TheFooter from "./components/TheFooter.js";
 
 export default class App extends Component {
   render() {
-    const theHeader = new TheHeader();
+    const theHeader = new TheHeader().el;
     const routerView = document.createElement("router-view");
+    const theFooter = new TheFooter().el;
 
-    this.el.append(theHeader.el, routerView);
+    this.el.append(theHeader, routerView, theFooter);
   }
 }
