c89d3a675c9d9683b1de52785405f8c79ca2e5b2
우정인 <wji0116@echomarketing.co.kr>
2025-08-12 17:29:16 +0900

    404추가

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ import { createRouter } from "../core/heropy.js";
 import Home from "./Home.js";
 import Movie from "./Movie.js";
 import About from "./About.js";
+import NotFound from "./NotFound.js";
 
 export default createRouter([
   { path: "#/", component: Home },
   { path: "#/movie", component: Movie },
   { path: "#/about", component: About },
+  { path: ".*", component: NotFound },
 ]);
